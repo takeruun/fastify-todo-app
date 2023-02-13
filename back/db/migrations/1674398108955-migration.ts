e3604947d390176c7897ac7baff69c5f5d54865c
takeru.hashimoto <hstake25@gmail.com>
2023-02-13 19:25:07 +0900

    run

--- back/db/migrations/1674398108955-migration.ts
+++ back/db/migrations/1674398108955-migration.ts
@@ -13,6 +13,7 @@ export class migration1674398108955 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE \`user\` DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\``);
+
     await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`email\``);
     await queryRunner.query(`ALTER TABLE \`user\` ADD \`email\` varchar(255) NOT NULL`);
     await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`email\` \`name\` varchar(255) NOT NULL`);
